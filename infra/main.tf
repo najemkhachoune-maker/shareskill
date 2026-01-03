b15c83b38fd03d5cede5a11453f73356289addad
najemkhachoune-maker <najem.khachoune@gmail.com>
2026-01-03 21:24:04 +0100

    Refactor Terraform configuration for EKS setup
    Updated security group and IAM role configurations to use existing VPC and subnets. Added recommended IAM policies for EKS worker nodes.

--- infra/main.tf
+++ infra/main.tf
@@ -13,21 +13,17 @@ provider "aws" {
 }
 
 # -------------------------------
-# VPC
+# VPC (reuse default)
 # -------------------------------
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
+data "aws_vpc" "default" {
+  default = true
+}
 
-  name = "challenge-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 # -------------------------------
@@ -36,7 +32,7 @@ module "vpc" {
 resource "aws_security_group" "challenge_sg" {
   name        = "challenge-sg"
   description = "Allow HTTP/HTTPS inbound"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     description = "HTTP"
@@ -81,22 +77,20 @@ resource "aws_iam_role" "challenge_node_role" {
   })
 }
 
-# Attach AmazonEKSFullAccess
-resource "aws_iam_role_policy_attachment" "eks_full_access" {
+# Attach recommended policies
+resource "aws_iam_role_policy_attachment" "eks_worker_node" {
   role       = aws_iam_role.challenge_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-# Attach IAMFullAccess
-resource "aws_iam_role_policy_attachment" "iam_full_access" {
+resource "aws_iam_role_policy_attachment" "ecr_read_only" {
   role       = aws_iam_role.challenge_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-# Attach AmazonEC2FullAccess
-resource "aws_iam_role_policy_attachment" "ec2_full_access" {
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent" {
   role       = aws_iam_role.challenge_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
 # -------------------------------
@@ -108,8 +102,8 @@ module "eks" {
 
   cluster_name    = "challenge-eks"
   cluster_version = "1.29"
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = data.aws_vpc.default.id
+  subnet_ids      = data.aws_subnets.default.ids
 
   eks_managed_node_groups = {
     default = {
@@ -145,4 +139,3 @@ output "security_group_id" {
 output "node_role_arn" {
   value = aws_iam_role.challenge_node_role.arn
 }
-
